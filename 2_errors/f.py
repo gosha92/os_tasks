1a5c5e0263d6a99301c98c470c5dea77b32c4142
gosha92 <alexeew.georgiy@gmail.com>
2014-01-28 01:24:59 +0600

    6

---
+++ 2_errors/f.py
@@ -0,0 +1,74 @@
+import os
+import re
+import sys
+import random
+
+def throw_var() :
+	seq = [0,1]
+	v = random.choice(seq)
+	return v
+
+def readdr_err() :
+	print ("Error was interrupted by walk")
+
+def sort_array(arr) :	
+	n = len(arr)
+	for j in range(1,n-1):
+		for t in range(0,n-j):
+			if int(arr[t]) > int(arr[t+1]) :
+				arr[t],arr[t+1] = arr[t+1],arr[t]
+	return (arr)
+
+path = "/num"
+
+if not os.path.exists(path):
+	print ("Error: This path isnt exist")
+	exit(0)
+
+# if not os.path.exists(path) or not throw_var():
+	# print ("Error: This path isnt exist")
+	# exit(0)
+	
+if not os.path.isdir(path):
+	print ("Error: This dir isnt exist")
+	exit(0)
+
+# if not os.path.isdir(path) or not throw_var():
+	# print ("Error: This dir isnt exist")
+	# exit(0)
+	
+try:
+	if throw_var():
+		raise NameError('walk')
+	for root, _ , files in os.walk(path, onerror = readdr_err()):
+		for f in files :
+			nextpath = os.path.join(str(root), str(f))
+			print (nextpath)		
+			try:
+				if throw_var():
+					raise Error('open')
+				nextfile = open(nextpath, 'r')
+			except:
+				print ('Error: Cant open ', nextpath)
+			else:		
+				try:
+					if throw_var():
+						raise Error('read')
+					result = []
+					for line in nextfile.readlines():
+						line = ' ' + line.strip('\n')+ ' '
+						line = line.replace(' ','  ')
+						p = re.compile('\s(\d+)\s')
+						result.extend(p.findall(line))
+					result=sort_array(result)
+					for match in result :
+						print (match)
+				except:
+					print ('Error: Cant take lines in file', nextpath)
+					
+				nextfile.close()
+except:
+	print ('Error: Cant walk')	
+		
+		
+
